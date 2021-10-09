80c84af23835cb80a0a9c56a3d0e63e46f7f512b
albtoul <albtoul-kaseem@hotmail.com>
2021-10-09 23:23:22 +0300

    jason

---
+++ app/src/main/java/com/example/jsonapp/APIInterface.kt
@@ -0,0 +1,14 @@
+package com.example.jsonapp
+
+import com.example.jsonapp.Datum
+import android.telecom.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface APIInterface {
+    @Headers("Content-Type: application/json")
+    @GET("eur.json")
+    fun getCurrency(): Call<Datum>?
+}
+
+
